2b915efc90eade76df92c42ac47842da98497125
Nick <nsagias@gmail.com>
2022-02-14 19:50:16 -0500

    create index.ts and add boilerplate

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+dotenv.config();
+
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
